c9e62f8ae560888063cad82b1d6954c37a93880b
Alexey Kolesnik <alexeyykolesnik@gmail.com>
2021-04-15 12:31:17 +0300

    1.3 lab

---
+++ 1_3b.py
@@ -0,0 +1,15 @@
+from nltk.book import *
+from pprint import pprint
+
+
+def dispersion_plot_demonstration():  # 1_3b
+    list_of_words = ['money', 'freedom', 'justice']
+    text7.dispersion_plot(list_of_words)
+
+
+def main():
+    dispersion_plot_demonstration()
+
+
+if __name__ == '__main__':
+    main()
