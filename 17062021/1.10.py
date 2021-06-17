da5250a0b039aefa16179a96196d81f6170140ab
leha_stira <leha_stira@ukr.net>
2021-06-17 05:44:44 +0300

    1.10

---
+++ 17062021/1.10.py
@@ -0,0 +1,13 @@
+from nltk.book import *
+
+
+def collocation_demostration():  # 1.10
+    text1.collocations()
+
+
+def main():
+    collocation_demostration()
+
+
+if __name__ == '__main__':
+    main()
